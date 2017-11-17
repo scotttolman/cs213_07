d0c292e101cd4afc346eaf54aa960d082f1d539f
scotttolman <tol16014@byui.edu>
2017-11-17 13:19:32 -0600

    update

--- Week07.js
+++ Week07.js
@@ -1,7 +1,31 @@
-var items = [];
-var prices = [];
+var total = [];
 
-function addToCart(item, amount) {
-    items.push(item);
-    prices.push(amount);
+function reset(form1, form2) {
+    document.getElementById(form1).reset();
+    document.getElementById(form2).reset();
+}
+
+function addToCart(form, input, price) {
+    if (/\d+/.test(document.getElementById(input).value)) {
+    total.push(document.getElementById(input).value * price);
+    document.getElementById(form).reset();
+    updateTotal();
+    }
+}
+
+function updateTotal() {
+    document.getElementById('total').innerHTML = addTotal();
+}
+
+function addTotal() {
+    var sum = 0;
+    for(i = 0; i < total.length; i++){
+        sum = sum + Number(total[i]);
+    }
+    return sum;
+}
+
+function emptyCart() {
+    total = [];
+    updateTotal();
 }
